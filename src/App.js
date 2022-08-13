bb8895d9a5c69693b1dd3827e3e81214749a7744
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-08-13 19:12:46 +0800

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import React from 'react'
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
 
+      PAGES
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
